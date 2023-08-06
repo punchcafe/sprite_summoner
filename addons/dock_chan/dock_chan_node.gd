dd859097b77c02b8bb101bb1cdcef224d1e02cd4
Luca Fruzza <lfruzza@hotmail.co.uk>
2023-08-06 23:23:15 +0100

    :sparkles: add SpriteSheetMeta data to the class

--- addons/dock_chan/dock_chan_node.gd
+++ addons/dock_chan/dock_chan_node.gd
@@ -29,7 +29,7 @@ func on_select_out_dir_click():
 	$SelectOutDirDialogue.visible = !$SelectOutDirDialogue.visible	
 	
 func on_press_generate():
-	print(_all_animation_frames(self.animation_name, $SheetDirectory.text))
+	print(SpriteSheetMeta.from(self.animation_name, $SheetDirectory.text))
 
 func _maybe_enable_generate():
 	var should_enable = $SheetDirectory.text != "" and animation_name != "" and $OutDirectory.text
@@ -43,26 +43,4 @@ func _process(delta):
 	
 func _regex_expression(sheet_name):
 	return "^" + sheet_name + "\\.([^\\.]+)\\.(\\d+)\\.(png)" + "$"
-	
-func _all_animation_frames(animation_name, directory_name):
-	var regex = RegEx.new()
-	regex.compile(_regex_expression(animation_name))
-	
-	var dir = DirAccess.open(directory_name)
-	var accumulator = []
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		var full_file_name = directory_name + "/" + file_name
-		while file_name != "":
-			if dir.current_is_dir():
-				accumulator += accumulator + _all_animation_frames(animation_name, full_file_name)
-			else:
-				if AnimationCellFile.is_sheet_cell(animation_name, file_name):
-					accumulator.append(AnimationCellFile.from_file(file_name, directory_name))
-			file_name = dir.get_next()
-			full_file_name = directory_name + "/" + file_name
-	else:
-		print("An error occurred when trying to access the path. " + directory_name)
-	return accumulator
 
--- addons/dock_chan/sprite_sheet_meta.gd
+++ addons/dock_chan/sprite_sheet_meta.gd
@@ -1,11 +1,43 @@
+class_name SpriteSheetMeta
 extends Resource
 
+var max_frames: int
+var animations := {}
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+static func from(sheet_name, directory_name):
+	var all_frames = _all_animation_frames(sheet_name, directory_name)
+	return all_frames.reduce(SpriteSheetMeta._reduce_animation_frames, SpriteSheetMeta.new())
+	
+static func _reduce_animation_frames(sprite_sheet_meta: SpriteSheetMeta, cell: AnimationCellFile) -> SpriteSheetMeta:
+	var cells = sprite_sheet_meta.animations.get(cell.animation_name, [])
+	cells.append(cell)
+	sprite_sheet_meta.animations[cell.animation_name] = cells
+	sprite_sheet_meta.max_frames = max(cell.cell_number, sprite_sheet_meta.max_frames)
+	return sprite_sheet_meta
 
+static func _all_animation_frames(animation_name, directory_name):
+	print("hello")
+	var dir = DirAccess.open(directory_name)
+	var accumulator = []
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		var full_file_name = directory_name + "/" + file_name
+		while file_name != "":
+			if dir.current_is_dir():
+				accumulator += accumulator + _all_animation_frames(animation_name, full_file_name)
+			else:
+				if AnimationCellFile.is_sheet_cell(animation_name, file_name):
+					accumulator.append(AnimationCellFile.from_file(file_name, directory_name))
+			file_name = dir.get_next()
+			full_file_name = directory_name + "/" + file_name
+	else:
+		print("An error occurred when trying to access the path. " + directory_name)
+	return accumulator
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func animation_count() -> int:
+	return self.animations.keys().size()
+
+func _to_string():
+	var template = "max_frames: 	{max_frames}\nanimation_count: {animation_count}\nsheet: {sheet}"
+	return template.format({"max_frames": self.max_frames, "animation_count": self.animation_count(), "sheet": self.animations})
